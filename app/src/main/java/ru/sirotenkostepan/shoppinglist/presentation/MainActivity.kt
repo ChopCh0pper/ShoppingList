a2f07c849cc11685cf1d125b8cb4dc204895b4d1
sks9 <sks9@tpu.ru>
2024-10-03 14:14:48 +0700

    create useCases

---
+++ app/src/main/java/ru/sirotenkostepan/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.sirotenkostepan.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.sirotenkostepan.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
